54fea32d48bc97eebbc0623736ed2edd372a3ca5
fakeYanss <yanshisangc@gmail.com>
2022-12-06 15:30:16 +0800

    sync script

--- sync.sh
+++ sync.sh
@@ -1,1 +1,11 @@
-for i in {0..6000}; do wget "https://game.gtimg.cn/images/lol/act/img/profileicon/$i.png" ; done
+#!/usr/bin/env bash
+
+for i in {0..6000}; do 
+    url="https://game.gtimg.cn/images/lol/act/img/profileicon/$i.png"
+    wget "$url" -O "avatar/$i.png" >/dev/null 2>&1
+    if [[ $? == 0 ]]; then
+        echo "Downloaded $i.png"
+    else
+        echo "Failed to download avatar=$i, url=$url"
+    fi
+done
